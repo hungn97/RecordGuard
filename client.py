4d2c56b4dbf1d55f9370107ca4c4c630edf38559
Dylan Banh <dylanb271@gmail.com>
2019-12-07 14:04:09 -0500

    client changes and added key for as and rs

--- client.py
+++ client.py
@@ -1,13 +1,16 @@
 import http.client
 from cryptography.fernet import Fernet
-import sys
 import json
 import time
 
-doc_as_key = open("docaskey.txt","r")
+doc_as_file = open("docaskey.txt","r")
+doc_as_key = doc_as_file.read().encode()
+doc_as_file.close()
+
 fernet_as = Fernet(doc_as_key)
 
 def create_auth_as():
+    "create the authentication package to send to as as a encrypted serialized json"
     doctor_id = input('Doctor ID')
     password = input('Doctor PW')
     patient_id = input('Patient ID')
@@ -24,22 +27,38 @@ def create_auth_as():
     return encrypted_json
 
 def receive_ticket(raw_response):
+    "receive the response from as and decrypt and extract the ticket as a string"
     plaintext_bytes = fernet_as.decrypt(raw_response)
     plaintext = plaintext_bytes.decode()
     return plaintext
 
 
-#get server ip and port
-server = sys.argv[1] #should be localhost 127.0.0.1
-port = sys.argv[2]  #8080(AS) or 8081(RS)
-
-conn = http.client.HTTPConnection(server, port)
-
 while True:
+    print("\n-------------")
+    # get server ip and port
+    while True:
+        cmd = input('enter \'server port\'\n>')
+        try:
+            server, port = cmd.split()
+            # localhost 127.0.0.1
+            # 8080(AS) or 8081(RS)
+            break;
+        except:
+            print("invalid input")
+
+    conn = http.client.HTTPConnection(server, port)
+
     if port == 8080:
         body = create_auth_as()
         conn.request("POST", "", body)
         raw_response = conn.getresponse()
         ticket = receive_ticket(raw_response)
 
+    if port == 8081:
+        body = create_auth_as()
+        conn.request("POST", "", body)
+        raw_response = conn.getresponse()
+        ticket = receive_ticket(raw_response)
 
+    print("Session completed\nClosing connection")
+    conn.close()
