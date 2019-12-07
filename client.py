70cd34e20af3ab21949aee081efa7f2d48442bab
Dylan Banh <dylanb271@gmail.com>
2019-12-07 17:33:28 -0500

    updated client, works now

--- client.py
+++ client.py
@@ -10,10 +10,10 @@ doc_as_file.close()
 fernet_as = Fernet(doc_as_key)
 
 def create_auth_as():
-    "create the authentication package to send to as as a encrypted serialized json"
-    doctor_id = input('Doctor ID')
-    password = input('Doctor PW')
-    patient_id = input('Patient ID')
+    """create the authentication package to send to as as a encrypted serialized json"""
+    doctor_id = input('enter Doctor ID\n>')
+    password = input('enter Doctor PW\n>')
+    patient_id = input('enter Patient ID\n>')
     timestamp = time.time()
     auth_json = {
         "doctorID": doctor_id,
@@ -27,7 +27,7 @@ def create_auth_as():
     return encrypted_json
 
 def receive_ticket(raw_response):
-    "receive the response from as and decrypt and extract the ticket as a string"
+    """receive the response from as and decrypt and extract the ticket as a string"""
     plaintext_bytes = fernet_as.decrypt(raw_response)
     plaintext = plaintext_bytes.decode()
     return plaintext
@@ -36,23 +36,29 @@ def receive_ticket(raw_response):
 while True:
     print("\n-------------")
     # get server ip and port
-    while True:
-        cmd = input('enter \'server port\'\n>')
-        try:
-            server, port = cmd.split()
-            # localhost 127.0.0.1
-            # 8080(AS) or 8081(RS)
-            break;
-        except:
-            print("invalid input")
-
+    # while True:
+    #     cmd = input('enter \'server port\'\n>')
+    #     try:
+    #         server, port = cmd.split()
+    #         # localhost
+    #         # 8080(AS) or 8081(RS)
+    #         break;
+    #     except:
+    #         print("invalid input")
+    server = 'localhost'
+    port = 8080
     conn = http.client.HTTPConnection(server, port)
+
+    headers = {'Content-type': 'text/plain'}
 
     if port == 8080:
         body = create_auth_as()
-        conn.request("POST", "", body)
-        raw_response = conn.getresponse()
-        ticket = receive_ticket(raw_response)
+        conn.request('POST', '/post', body, headers)
+        http_response = conn.getresponse()
+        message = http_response.read()
+        # print(message.decode())
+        ticket = receive_ticket(message)
+        # print(ticket)
 
     if port == 8081:
         body = create_auth_as()
