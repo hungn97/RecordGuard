ed6544eecd9244511b39ee2ed5bca92100566cef
Dylan Banh <dylanb271@gmail.com>
2019-12-07 16:17:36 -0500

    initial changes to as

--- authserver.py
+++ authserver.py
@@ -1,39 +1,60 @@
-import socket
+import http.server
+import socketserver
+from cryptography.fernet import Fernet
 import json
+import time
 
-__public_key = 12345
-__private_key = 54321
-__shared_key = 11111
+doc_as_file = open("docaskey.txt","r")
+doc_as_key = doc_as_file.read().encode()
+doc_as_file.close()
+fernet_as = Fernet(doc_as_key)
 
-conn = sqlite3.connect('doctor_data.db')
+as_rs_file = open("asrskey.txt","r")
+as_rs_key = as_rs_file.read().encode()
+as_rs_file.close()
+fernet_rs = Fernet(doc_as_key)
 
-def server_hello():
-    "Creates JSON containing server's public key"
-    hello = {
-        "publicKey": __public_key
-    }
-    serialized_hello = json.dumps(hello)
-    return serialized_hello
+# dbconn = sqlite3.connect('doctor_data.db')
 
-def decrypt_message(ciphertext):
-    "Decrypt ciphertext into plaintext using private key"
-    plaintext = ciphertext
-    return plaintext
 
-def verify_ds(ds):
-    "Check that digital signature affirms identity"
+class S(http.server.BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
 
-    return
+    def do_GET(self):
+        self._set_headers()
+        print("getting")
+        self.wfile.write("get")
 
-def verify_credentials(message):
-    "Access database and fetch user credetials for verification"
-    #could use actual sql database or just a csv file to mock it
-    #need a function to seach through sql database and return true if found
-    #found = verify_database(message.doctorID, message.doctorPW, message.securityAns, message.patientID)
-    if found and verify_ds(message.ds):
-        return True
-    else:
-        return False
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        print("posting")
+        self.wfile.write("post")
+
+
+
+# def decrypt_message(ciphertext):
+#     "Decrypt ciphertext into plaintext using private key"
+#     plaintext = ciphertext
+#     return plaintext
+#
+# def verify_ds(ds):
+#     "Check that digital signature affirms identity"
+#
+#     return
+
+# def verify_credentials(message):
+#     "Access database and fetch user credetials for verification"
+#     #could use actual sql database or just a csv file to mock it
+#     #need a function to seach through sql database and return true if found
+#     #found = verify_database(message.doctorID, message.doctorPW, message.securityAns, message.patientID)
+#     if found and verify_ds(message.ds):
+#         return True
+#     else:
+#         return False
 
 def create_ticket(message):
     ticket = {
@@ -44,31 +65,27 @@ def create_ticket(message):
     return ticket
 
 def encrypt_ticket(ticket):
-    "Encrypts ticket as authentication for client"
-    #use encryption from package
+    "Encrypts ticket as authentication for client to send to record server"
+    encrypted_ticket = fernet_rs.encrypt(ticket)
     return encrypted_ticket
 
-soc = socket.socket()
-print("Socket created")
 
-port = 10000
 
-soc.bind(('', port))
-print("Socket binded to %s" %port)
+(server, port) = ('127.0.0.1', 8080)
 
-soc.listen(1)
-print("Socket is listening")
+httpd = http.server.HTTPServer((server, port), S)
+print("serving at port", port)
+httpd.serve_forever()
 
-while True:
-    client, addr = soc.accept()
-    print("Connection from", addr)
-    client.send(server_hello())
-    message = client.recv(1024)
-    if message is not None:
-        decrypted_message = decrypt_message(message)
-        json_message = json.loads(decrypted_message)
-        if verify_creditials(json_message):
-                ticket = create_ticket(json_message)
-                serialized_ticket = json.dumps(ticket)
-                encrypted_ticket = encrypt_ticket(serialized_ticket)
-                client.send(encrypted_ticket)
+# while True:
+#     client, addr = soc.accept()
+#     print("Connection from", addr)
+#     message = client.recv(1024)
+#     if message is not None:
+#         decrypted_message = decrypt_message(message)
+#         json_message = json.loads(decrypted_message)
+#         if verify_creditials(json_message):
+#                 ticket = create_ticket(json_message)
+#                 serialized_ticket = json.dumps(ticket)
+#                 encrypted_ticket = encrypt_ticket(serialized_ticket)
+#                 client.send(encrypted_ticket)
